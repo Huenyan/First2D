2d9ce520ef3aa8aade10aa216ee101a2b54eacbb
Ronyan Lucietti <ronyan_lucietti@hotmail.com>
2017-10-04 10:59:35 -0300

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	Animator myAnimator;
+	public float speed = 10;
+	private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		myAnimator.SetFloat ("speed", Mathf.Abs (horizontal));
+
+		transform.Translate (Time.deltaTime * (speed * horizontal), 0, 0);
+		Flip (horizontal);
+
+
+	}
+
+	void Flip(float horizontal) {
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+			facingRight = !facingRight;
+			SpriteRenderer flipX = GetComponent<SpriteRenderer> ();
+			flipX.flipX = !facingRight;
+		}
+	
+	}
+
+
+}
